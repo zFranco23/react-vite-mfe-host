42fa4db297b5f31f0f34e43342500b44bf88a444
zFranco <francoheflo@gmail.com>
2025-07-28 00:00:44 -0500

    feat: initial setup for host

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-bold text-3xl">Mfe host app</h1>
+      <p className="text-lg">This is the boilerplate of mfe-host</p>
+    </div>
+  );
+};
+
+export default App;
