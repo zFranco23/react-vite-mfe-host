6f0968529a8725aa2851d886783d8dc2579c2b8b
zFranco <francoheflo@gmail.com>
2025-07-28 23:07:52 -0500

    feat: update mfe remote scope

--- src/mfe/remotes.ts
+++ src/mfe/remotes.ts
@@ -5,8 +5,10 @@ export const REMOTES_DEFINITION: RemoteDefinition[] = [
     name: "MFE_1",
     scopes: {
       local: "http://localhost:3001/remoteEntry.js",
-      development: "http://localhost:3001/remoteEntry.js",
-      production: "http://localhost:3001/remoteEntry.js",
+      development:
+        "https://react-vite-example-remote-1.netlify.app/remoteEntry.js",
+      production:
+        "https://react-vite-example-remote-1.netlify.app/remoteEntry.js",
     },
   },
 ];
